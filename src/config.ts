3c8515ccb166d7e2dd68551344ed2915f0d07d97
zakaria <zakariabaouali@outlook.com>
2024-06-07 00:32:16 +0100

    update config file

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,5 @@ env.config();
 
 export const SERVER_PORT = process.env.PORT || 3000;
 export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || "";
+export const STRIPE_WEBHOOK_KEY = "";
+//whsec_48e5a49421890aabc11f43c630e19aaa200e82b93dd4d329ccdc11cddf441ca5
